38478121b60b160d535f68ad7fcf685993bdab75
Hamailkhan <hamailkhan213@gmail.com>
2024-03-02 22:19:29 +0500

    This is blog app

--- BlogHome/blogHome.js
+++ BlogHome/blogHome.js
@@ -105,7 +105,7 @@ onChildAdded(ref(rdb, "blogs/"), (data) => {
     });
   } else {
     let cardHtml = `
- <div class="card w-96 bg-base-100 shadow-xl co" id="card">
+ <div class="card bg-base-100 shadow-xl co" id="card">
    <figure>
      <img src="${blogPicture}"/>
    </figure>
@@ -171,53 +171,5 @@ search.addEventListener("input", () => {
     } catch (error) {
       console.error(error);
     }
-  }
-});
-
-const createBlogBtn = document.getElementById("createBlogBtn");
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth < 425) {
-    createBlogBtn.innerHTML = `<svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <line x1="12" y1="5" x2="12" y2="19"></line>
-              <line x1="5" y1="12" x2="19" y2="12"></line>
-            </svg>`;
-    createBlogBtn.classList.add("btn-square");
-  } else {
-    createBlogBtn.innerHTML = `Create Blog`;
-    createBlogBtn.classList.remove("btn-square");
-  }
-});
-
-window.addEventListener("load", () => {
-  if (window.innerWidth < 425) {
-    createBlogBtn.innerHTML = `<svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <line x1="12" y1="5" x2="12" y2="19"></line>
-              <line x1="5" y1="12" x2="19" y2="12"></line>
-            </svg>`;
-    createBlogBtn.classList.add("btn-square");
-  } else {
-    createBlogBtn.innerHTML = `Create Blog`;
-    createBlogBtn.classList.remove("btn-square");
   }
 });
--- globaljs.js
+++ globaljs.js
@@ -6,3 +6,53 @@ window.addEventListener("load", () => {
   loadCon.remove();
   document.body.style.overflow = "auto";
 });
+
+const createBlogBtn = document.getElementById("createBlogBtn");
+
+if (createBlogBtn) {
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 425) {
+      createBlogBtn.innerHTML = `<svg
+      xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              >
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>`;
+      createBlogBtn.classList.add("btn-square");
+    } else {
+      createBlogBtn.innerHTML = `Create Blog`;
+      createBlogBtn.classList.remove("btn-square");
+    }
+  });
+
+  window.addEventListener("load", () => {
+    if (window.innerWidth < 425) {
+      createBlogBtn.innerHTML = `<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    >
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>`;
+      createBlogBtn.classList.add("btn-square");
+    } else {
+      createBlogBtn.innerHTML = `Create Blog`;
+      createBlogBtn.classList.remove("btn-square");
+    }
+  });
+}
